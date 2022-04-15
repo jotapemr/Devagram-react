bc53b9553f75ec1bf68acb672bd52bdcab2d68e9
jotapemr <101481431+jotapemr@users.noreply.github.com>
2022-04-15 17:54:41 -0300

    componente botão

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Botao from '../componentes/botao'
 
 export default function Home() {
   return (
+    <>
     <h1>Olá mundo</h1>
+    <div style={{width:200}}>
+      <Botao texto={'Login'} cor='primaria' manipularClique={() => console.log('Botão clicado')}/>
+    </div>
+    </>
   )
 }
