ac77e9d9eefda451ed803f196bc2faa4d9d95b71
jotapemr <101481431+jotapemr@users.noreply.github.com>
2022-04-22 12:49:34 -0300

    começo perfil

--- componentes/cabecalhoPerfil/index.js
+++ componentes/cabecalhoPerfil/index.js
@@ -2,37 +2,93 @@ import imgSetaEsquerda from '../../public/imagens/setaEsquerda.svg'
 import cabecalhoComAcoes from '../cabecalhoComAcoes'
 import Botao from '../botao'
 import Avatar from '../avatar'
+import { useEffect, useState } from 'react'
+import UsuarioService from '../../services/UsuarioService'
+import { useRouter } from 'next/router'
+
+const usuarioService = new UsuarioService()
 
 export default function CabecalhoPerfil({
-
+    usuario
 }) {
+    const[estaSeguindoUsuario,  setEstaSeguindoUsuario] = useState(false)
+    const [quantidadeSeguidores, setQuantidadeSeguidores] = useState(0)
+    const router = useRouter()
+
+    useEffect(() => {
+        if(usuario){
+            return
+        }
+        setEstaSeguindoUsuario(usuario.segueEsseUsuario)
+        setQuantidadeSeguidores(usuario.seguidores)
+    }, [usuario])
+
+    const obterTextoBotaoSeguir = () => {
+        if(estaSeguindoUsuario){
+            return 'Deixar de seguir'
+        }
+        return 'Seguir'
+    }
+
+    const obterCorDoBotaoSeguir = () => {
+        if(estaSeguindoUsuario){
+            return 'invertido'
+        }
+        return 'primaria'
+    }
+
+    const manipularCliqueBotaoSeguir = async () => {
+        try{
+            await usuarioService.alternarSeguir(usuario._id)
+            setEstaSeguindoUsuario(!estaSeguindoUsuario)
+            setQuantidadeSeguidores(
+                estaSeguindoUsuario 
+                ? (quantidadeSeguidores - 1)
+                : (quantidadeSeguidores + 1)
+
+            )
+        }catch(erro){
+            aler(`Errom ao seguir ou deixar de seguir`)
+        }
+    }
+
+    const aoClicarSetaEsquerda = () => {
+        router.back()
+    }
+
     return(
+
+
         <div className='cabecalhoPerfil largura30pctDesktop'>
             <CabecalhoComAcoes
                 iconeEsquerda={imgSetaEsquerda}
+                aoClicarSetaEsquerda={aoClicarSetaEsquerda}
                 titulo={usuario.nome} 
             />
+
+            <hr className='bordaCabecalhoPerfil'/>
 
             <div className='statusPerfil'>
                 <Avatar src={usuario.avatar}/>
                 <div className='informacoesPerfil'>
                     <div className='statusContainer'>
                         <div className='status'>
-                            <strong>15</strong>
+                            <strong>{usuario.publicacoes}</strong>
                             <span>Publicações</span>
                         </div>
                         <div className='status'>
-                            <strong>120</strong>
+                            <strong>{quantidadeSeguidores}</strong>
                             <span>Seguidores</span>
                         </div>
                         <div className='status'>
-                            <strong>135</strong>
+                            <strong>{usuario.seguindo}</strong>
                             <span>Seguindo</span>
                         </div>
                     </div>
                     <Botao
-                        texto={'Seguir'}
-                        cor='primaria'
+                        texto={obterTextoBotaoSeguir()}
+                        cor={obterCorDoBotaoSeguir()}
+                        manipularClique={manipularCliqueBotaoSeguir}
                     />
                 </div>
             </div>
--- pages/perfil/[id]/index.js
+++ pages/perfil/[id]/index.js
@@ -5,14 +5,30 @@ import {UseRouter} from 'next/router'
 import UsuarioService from '../../../services/UsuarioService'
 import CabecalhoPerfil from '../../../componentes/cabecalhoPerfil'
 
+
+const usuarioService = new UsuarioService();
+
  function Perfil({usuarioLogado}){
     const [usuario, setUsuario] = useState({})
     const router = useRouter()
 
+    const obterPerfil = async () => {
+        try{
+          const {data} = await usuarioService.obterPerfil(idUsuario)
+          return data
+        }catch(error){
+            alert(`Erro ao obter perfil do usuário`)
+        }
+    }
+
     useEffect (async () => {
-        setUsuario({
-            //usuario mockado (por enquanto)
-        })
+        if(!router.query.id){
+            return
+        }
+
+        const dadosPerfil = await obterPerfil(router.query.id)
+
+        setUsuario(dadosPerfil)
     }, [router.query.id])
 
     return(
@@ -22,7 +38,10 @@ import CabecalhoPerfil from '../../../componentes/cabecalhoPerfil'
                 usuario={usuario}
             />
             
-            <Feed usuarioLogado={usuarioLogado}/>
+            <Feed 
+                usuarioLogado={usuarioLogado}
+                usuarioPerfil={usuario}
+            />
         </div>
     ) 
 }
