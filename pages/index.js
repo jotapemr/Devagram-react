d635b0c50c2072bab856560310dc0c9b179b6667
jotapemr <101481431+jotapemr@users.noreply.github.com>
2022-04-23 18:08:20 -0300

    correção

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Home from "../componentes/home";
 import Login from "../componentes/login";
-import UsuarioService from "../services/UsuarioService";
+import UsuarioService from "../services/usuarioService";
 import comAutorizacao from '../hoc/comAutorizacao'
 
 const usuarioService = new UsuarioService();
